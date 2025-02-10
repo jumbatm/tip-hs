9440e0e4ea3fe44ea4f75fbe1b12fdd24d52ebab
jumbatm <jumbatm@gmail.com>
2025-02-10 17:14:46 +1000

    Initial commit

---
+++ src/Parser.hs
@@ -0,0 +1,20 @@
+module Parser (parse) where
+
+-- Our parser type. We return pairs of (the parsed object, the remaining
+-- string). We have a list as we're able to handle ambiguous grammars and
+-- return all possible parse trees.
+newtype Parser a = Parser (String -> [(a, String)])
+
+data AST = AST deriving (Show)
+
+-- Actual function we export.
+parse :: String -> Maybe AST
+parse _s = Just AST
+
+-- TODO: Fill in.
+instance Functor Parser
+
+-- TODO: Fill in.
+instance Applicative Parser
+
+instance Monad Parser
