407712ad4e7a2456d72780d01adaeaf313d2dd3c
jumbatm <30644300+jumbatm@users.noreply.github.com>
2025-03-03 16:50:07 +1000

    WIP

--- src/Parser/Internal.hs
+++ src/Parser/Internal.hs
@@ -1,4 +1,4 @@
-module Parser.Internal (parse, charP, stringP, spanP, identifierP, runParser, Decl (..)) where
+module Parser.Internal (parse, token, keyword, satisfyWhile, identifierP, runParser, Decl (..)) where
 
 import Control.Applicative
 import Data.Char
@@ -62,20 +62,11 @@ instance Alternative Parser where
   empty = Parser $ const Nothing
   p <|> q = Parser $ \s -> runParser p s <|> runParser q s
 
-charP :: Char -> Parser Char
-charP e = Parser $ f
-  where
-    f :: String -> Maybe (Char, String)
-    f (c : cs)
-      | c == e = Just (c, cs)
-      | otherwise = Nothing
-    f [] = Nothing
+ws :: Parser String
+ws = satisfyWhile isSpace
 
-stringP :: String -> Parser String
-stringP = sequenceA . fmap charP
-
-predP :: (Char -> Bool) -> Parser Char
-predP p = Parser f
+satisfy :: (Char -> Bool) -> Parser Char
+satisfy p = Parser f
   where
     f :: String -> Maybe (Char, String)
     f [] = Nothing
@@ -83,51 +74,95 @@ predP p = Parser f
       | p c = Just (c, cs)
       | otherwise = Nothing
 
-spanP :: (Char -> Bool) -> Parser String
-spanP = many . predP
+satisfyWhile :: (Char -> Bool) -> Parser [Char]
+satisfyWhile = many . satisfy
 
-wsP :: Parser String
-wsP = spanP isSpace
-
--- Allow any amount of whitespace after the parser.
-lexeme :: Parser a -> Parser a
-lexeme p = p <* wsP
 
 sepBy :: Parser a -> Parser b -> Parser [a]
 sepBy p q = (:) <$> p <*> many (q *> p) <|> pure []
 
+-- Allow any amount of whitespace after the parser.
+lexeme :: Parser a -> Parser a
+lexeme p = p <* ws
+
+token :: Char -> Parser Char
+token e = lexeme $ Parser $ f
+  where
+    f :: String -> Maybe (Char, String)
+    f (c : cs)
+      | c == e = Just (c, cs)
+      | otherwise = Nothing
+    f [] = Nothing
+
+keyword :: String -> Parser String
+keyword = lexeme . sequenceA . fmap token
+
 -- TIP Parsing.
 
 tipProgramP :: Parser TipProgram
-tipProgramP = TipProgram <$> many functionP
+tipProgramP = TipProgram <$> (ws *> many functionP)
+
+expressionP :: Parser Expression
+expressionP = termP
+
+termOpP :: Parser Op
+termOpP = lexeme $ Add <$ token '+'
+      <|> Subtract <$ token '-'
+      <|> GreaterThan <$ token '>'
+      <|> Equal <$ keyword "=="
+
+factorOpP :: Parser Op
+factorOpP = lexeme $ Multiply <$ token '*'
+      <|> Divide <$ token '/'
+
+termP :: Parser Expression
+termP = ((\lhs op rhs -> Binary op lhs rhs) <$> factorP <*> termOpP <*> factorP) <|> factorP
+
+factorP' :: Parser (Maybe (Op, Expression))
+factorP' = optional ((\op rhs -> (op, rhs)) <$> factorOpP <*> factorP)
+
+factorP :: Parser Expression
+factorP = f <$> ( intP <|> idP <|> (token '(' *> expressionP <* token ')') ) <*> factorP'
+  where
+    f :: Expression -> Maybe (Op, Expression) -> Expression
+    f lhs op_rhs = case op_rhs of
+                        Nothing -> lhs
+                        Just (op, rhs) -> Binary op lhs rhs
+
+intP :: Parser Expression
+intP = lexeme $ Int <$> read <$> ((:) <$> satisfy isDigit <*> satisfyWhile isDigit)
+
+idP :: Parser Expression
+idP = lexeme $ Id <$> identifierP
 
 statementP :: Parser Statement
-statementP = (variableDeclarationStmt <|> outputStmt <|> ifStmt <|> returnStmt) <* lexeme (charP ';')
-  where
-    variableDeclarationStmt :: Parser Statement
-    variableDeclarationStmt = VariableDeclaration <$> (lexeme (stringP "var") *> lexeme identifierP)
+statementP = lexeme $ (variableDeclarationP <|> outputP <|> ifP <|> returnP) <* lexeme (token ';')
 
-    outputStmt :: Parser Statement
-    outputStmt = undefined
+variableDeclarationP :: Parser Statement
+variableDeclarationP = lexeme $ VariableDeclaration <$> (lexeme (keyword "var") *> identifierP)
 
-    ifStmt :: Parser Statement
-    ifStmt = undefined
+outputP :: Parser Statement
+outputP = Output <$> (keyword "output" *> expressionP)
 
-    returnStmt :: Parser Statement
-    returnStmt = undefined
+ifP :: Parser Statement
+ifP = If <$> (keyword "if" *> token '(' *> expressionP <* token ')') <*> (token '{' *> many statementP <* token '}') <*> optional (keyword "else" *> token '{' *> many statementP <* token '}')
+
+returnP :: Parser Statement
+returnP = Return <$> (keyword "return" *> optional expressionP)
 
 identifierP :: Parser String
 identifierP = do
-  i <- predP isAlpha
-  is <- spanP (\x -> isDigit x || isAlpha x)
+  i <- satisfy isAlpha
+  is <- satisfyWhile (\x -> isDigit x || isAlpha x)
   return $ i : is
 
 functionP :: Parser Decl
 functionP = do
   functionName <- lexeme identifierP
-  _ <- lexeme $ charP '('
-  functionParams <- lexeme identifierP `sepBy` lexeme (charP ',')
-  _ <- lexeme $ charP ')'
-  _ <- lexeme $ charP '{'
-  _ <- lexeme $ charP '}'
-  return $ Function functionName functionParams []
+  _ <- token '('
+  functionParams <- lexeme identifierP `sepBy` token ','
+  _ <- token ')'
+  _ <- token '{'
+  statements <- many statementP
+  _ <- token '}'
+  return $ Function functionName functionParams statements
