ca71a6ab230e69dd087fe016a66dba7c9032b55a
jumbatm <jumbatm@gmail.com>
2025-03-07 17:30:19 +1000

    Split TipParsing out into separate module.

--- src/Parser/Internal.hs
+++ src/Parser/Internal.hs
@@ -1,4 +1,4 @@
-module Parser.Internal (parse, token, keyword, satisfyWhile, identifierP, runParser, Decl (..)) where
+module Parser.Internal (Parser, sepBy, ws, lexeme, satisfy, token, keyword, satisfyWhile, runParser) where
 
 import Control.Applicative
 import Data.Char
@@ -9,35 +9,6 @@ import Data.Char
 newtype Parser a = Parser
   { runParser :: String -> Maybe (a, String)
   }
-
-newtype TipProgram = TipProgram [Decl] deriving (Show)
-
-data Decl
-  = Identifier String
-  | Function String [String] [Statement]
-  deriving (Show, Eq)
-
-data Op = Add | Subtract | Multiply | Divide | GreaterThan | Equal deriving (Show, Eq)
-
-data Statement
-  = VariableDeclaration String
-  | Output Expression
-  | If Expression [Statement] (Maybe [Statement])
-  | Return (Maybe Expression)
-  deriving (Show, Eq)
-
-data Expression
-  = Int Int
-  | Id String
-  | Binary Op Expression Expression
-  | Call Expression [Expression]
-  deriving (Show, Eq)
-
--- Actual function we export.
-parse :: String -> Maybe TipProgram
-parse s = do
-  (ast, rest) <- runParser tipProgramP s
-  if null rest then Just ast else Nothing
 
 instance Functor Parser where
   fmap f p = Parser $ \s -> do
@@ -97,72 +68,3 @@ token e = lexeme $ Parser $ f
 keyword :: String -> Parser String
 keyword = lexeme . sequenceA . fmap token
 
--- TIP Parsing.
-
-tipProgramP :: Parser TipProgram
-tipProgramP = TipProgram <$> (ws *> many functionP)
-
-expressionP :: Parser Expression
-expressionP = termP
-
-termOpP :: Parser Op
-termOpP = lexeme $ Add <$ token '+'
-      <|> Subtract <$ token '-'
-      <|> GreaterThan <$ token '>'
-      <|> Equal <$ keyword "=="
-
-factorOpP :: Parser Op
-factorOpP = lexeme $ Multiply <$ token '*'
-      <|> Divide <$ token '/'
-
-termP :: Parser Expression
-termP = ((\lhs op rhs -> Binary op lhs rhs) <$> factorP <*> termOpP <*> factorP) <|> factorP
-
-factorP' :: Parser (Maybe (Op, Expression))
-factorP' = optional ((\op rhs -> (op, rhs)) <$> factorOpP <*> factorP)
-
-factorP :: Parser Expression
-factorP = f <$> ( intP <|> idP <|> (token '(' *> expressionP <* token ')') ) <*> factorP'
-  where
-    f :: Expression -> Maybe (Op, Expression) -> Expression
-    f lhs op_rhs = case op_rhs of
-                        Nothing -> lhs
-                        Just (op, rhs) -> Binary op lhs rhs
-
-intP :: Parser Expression
-intP = lexeme $ Int <$> read <$> ((:) <$> satisfy isDigit <*> satisfyWhile isDigit)
-
-idP :: Parser Expression
-idP = lexeme $ Id <$> identifierP
-
-statementP :: Parser Statement
-statementP = lexeme $ (variableDeclarationP <|> outputP <|> ifP <|> returnP) <* lexeme (token ';')
-
-variableDeclarationP :: Parser Statement
-variableDeclarationP = lexeme $ VariableDeclaration <$> (lexeme (keyword "var") *> identifierP)
-
-outputP :: Parser Statement
-outputP = Output <$> (keyword "output" *> expressionP)
-
-ifP :: Parser Statement
-ifP = If <$> (keyword "if" *> token '(' *> expressionP <* token ')') <*> (token '{' *> many statementP <* token '}') <*> optional (keyword "else" *> token '{' *> many statementP <* token '}')
-
-returnP :: Parser Statement
-returnP = Return <$> (keyword "return" *> optional expressionP)
-
-identifierP :: Parser String
-identifierP = do
-  i <- satisfy isAlpha
-  is <- satisfyWhile (\x -> isDigit x || isAlpha x)
-  return $ i : is
-
-functionP :: Parser Decl
-functionP = do
-  functionName <- lexeme identifierP
-  _ <- token '('
-  functionParams <- lexeme identifierP `sepBy` token ','
-  _ <- token ')'
-  _ <- token '{'
-  statements <- many statementP
-  _ <- token '}'
-  return $ Function functionName functionParams statements
