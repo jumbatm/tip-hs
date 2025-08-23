bbf916721b7b784dbd03edad41027bc8fda61a2e
jumbatm <30644300+jumbatm@users.noreply.github.com>
2025-08-23 22:26:58 +1000

    Remove redundant import.

--- src/Parser/TipParser.hs
+++ src/Parser/TipParser.hs
@@ -3,7 +3,6 @@ module Parser.TipParser (parse, TipProgram, ParseResult (..), identifierP) where
 import Parser.Internal
 import Control.Applicative
 import Data.Char
-import Data.Kind
 
 newtype TipProgram = TipProgram [Decl] deriving (Show)
 
