2fc0a1d3a03d0b845b8f6a3c280910f82cc2f75a
jumbatm <30644300+jumbatm@users.noreply.github.com>
2025-09-14 09:40:04 +1000

    WIP: Add function to grab nextChar

--- src/Parser/Internal.hs
+++ src/Parser/Internal.hs
@@ -11,7 +11,7 @@ data ParseResult a = ParseError String | ParseOk a deriving (Show, Eq)
 
 instance Functor ParseResult where
   fmap f (ParseOk v) = ParseOk $ f v
-  fmap _ (ParseError m) = ParseError m
+  fmap _(ParseError m) = ParseError m
 
 instance Applicative ParseResult where
   pure = ParseOk
@@ -20,7 +20,7 @@ instance Applicative ParseResult where
   ParseOk f <*> r = fmap f r
 
 instance Monad ParseResult where
-  ParseError m >>= _ = ParseError m
+  ParseError m  >>= _ = ParseError m
   ParseOk v >>= f = f v
 
 instance MonadFail ParseResult where
@@ -34,19 +34,20 @@ instance Alternative ParseResult where
   -- Is the rightmost ParseError the right one?
   (ParseError _) <|> (ParseError rm) = ParseError rm
 
+
 -- Our parser type. We return pairs of (the parsed object, the remaining
 -- string). We have a list as we're able to handle ambiguous grammars and
 -- return all possible parse trees.
-newtype (Monad m) => Parser i m o = Parser
+newtype Monad m => Parser i m o = Parser
   { runParser :: [i] -> m (o, [i])
   }
 
-instance (Monad m) => Functor (Parser i m) where
+instance Monad m => Functor (Parser i m) where
   fmap f p = Parser $ \s -> do
     (v, s') <- runParser p s
     return (f v, s')
 
-instance (Monad m) => Applicative (Parser i m) where
+instance Monad m => Applicative (Parser i m) where
   pure v = Parser $ \s -> pure (v, s)
   pf <*> pv = Parser $ \s -> do
     -- TODO: Which order should this actually be in? Unwrap f first or v? Does
@@ -55,7 +56,7 @@ instance (Monad m) => Applicative (Parser i m) where
     (v, s'') <- runParser pv s'
     return (f v, s'')
 
-instance (Monad m) => Monad (Parser i m) where
+instance Monad m => Monad (Parser i m) where
   pv >>= pf = Parser $ \s -> do
     (v, s') <- runParser pv s
     runParser (pf v) s'
@@ -63,6 +64,7 @@ instance (Monad m) => Monad (Parser i m) where
 instance (Alternative m, Monad m) => Alternative (Parser i m) where
   empty = Parser $ const empty
   p <|> q = Parser $ \s -> runParser p s <|> runParser q s
+
 
 satisfy :: (Alternative m, Monad m) => (i -> Bool) -> Parser i m i
 satisfy p = Parser f
@@ -74,13 +76,14 @@ satisfy p = Parser f
       | p c = pure (c, cs)
       | otherwise = empty
 
+
 satisfyWhile :: (Alternative m, Monad m) => (i -> Bool) -> Parser i m [i]
 satisfyWhile = many . satisfy
 
 sepBy :: (Alternative m, Monad m) => Parser i m o -> Parser i m o' -> Parser i m [o]
 sepBy p q = (:) <$> p <*> many (q *> p) <|> pure []
 
-parseError :: (MonadFail m) => String -> Parser i m o
+parseError :: MonadFail m => String -> Parser i m o
 parseError msg = Parser $ pure (fail msg)
 
 -- Parser on Strings.
@@ -95,6 +98,13 @@ lexeme p = p <* ws
 
 token :: (Alternative m, Monad m) => Char -> CharParser m Char
 token e = lexeme . Parser $ f
+nextChar :: (Monad m, Alternative m) => CharParser m Char
+nextChar = Parser f
+  where
+    -- TODO: Automatically update source location.
+    f (c : cs) = pure (c, cs)
+    f [] = empty
+
   where
     f (c : cs)
       | c == e = pure (c, cs)
