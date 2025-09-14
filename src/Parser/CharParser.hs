4f147b9487b3bbb4f08d51ad8db0ec8fa501c9ff
jumbatm <30644300+jumbatm@users.noreply.github.com>
2025-09-14 10:45:08 +1000

    Address FIXME about writing explicit signature.
    Turns out we just needed ScopedTypeVariables.

--- src/Parser/CharParser.hs
+++ src/Parser/CharParser.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE ScopedTypeVariables #-}
 module Parser.CharParser where
 
 import Parser.Internal
@@ -7,11 +8,10 @@ import Data.Char
 -- Parser on Strings.
 type CharParser = Parser Char
 
-satisfy :: (Alternative m, Monad m) => (i -> Bool) -> Parser i m i
+satisfy :: forall i. forall m. (Alternative m, Monad m) => (i -> Bool) -> Parser i m i
 satisfy p = Parser f
   where
-    -- FIXME: Why can't I write this signature?
-    -- f :: [i] -> Maybe (i, [i])
+    f :: Alternative m => [i] -> m (i, [i])
     f [] = empty
     f (c : cs)
       | p c = pure (c, cs)
