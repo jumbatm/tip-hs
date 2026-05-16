f07d359e2400d8973ed30f1af8a5ce34a09b9fc4
jumbatm <jumbatm@gmail.com>
2026-05-16 14:50:33 +1000

    Implement `try` combinator

--- test/ParserSpec.hs
+++ test/ParserSpec.hs
@@ -71,7 +71,9 @@ spec = do
     it "returns Nothing if both parsers fail" $ do
       testRunParser (keyword "foo" <|> keyword "bar") "bazqux" `shouldBe` ParseError Empty (SourceLocation (1, 1)) (fromList ["foo", "bar"])
     it "commits to the first parser if we got a partial match" $ do
-      testRunParser (string "ABC" <|> string "DEF") "ABG" `shouldBe` ParseError Consumed (SourceLocation (1, 3)) (fromList ["ABC"])
+      testRunParser (string "ABC" <|> string "ABG") "ABG" `shouldBe` ParseError Consumed (SourceLocation (1, 3)) (fromList ["ABC"])
+    it "allows backtracking if the try combinator is used" $ do
+      testRunParser (try (string "ABC") <|> string "ABG") "ABG" `shouldBe` ParseOk ("ABG", "")
 
   describe "satisfyWhile" $ do
     it "correctly splits string starting with chars which satisfy the predicate" $ do
