965e98dde3650b581f02469876c6aec3997f5b71
jumbatm <jumbatm@gmail.com>
2026-08-29 20:38:22 +1000

    Split out a lexer module with terminals

---
+++ src/Parser/TipLexer.hs
@@ -0,0 +1,63 @@
+module Parser.TipLexer where
+
+import Control.Applicative
+import Data.Char
+import Data.Functor.Identity
+import Data.Set (singleton)
+import Parser.CharParser (CharParser, CharParserState (..), satisfy, satisfyWhile, string, (<?>))
+import Parser.Internal
+
+type TipParser = CharParser Identity
+
+lineComment :: TipParser String
+lineComment = string "//" *> (string "\\\n" <|> satisfyWhile (/= '\n'))
+
+-- Whitespace and comments consumer.
+ws :: Parser CharParserState Identity [String]
+ws = many (lineComment <|> ((: []) <$> satisfy isSpace))
+
+-- Allow any amount of whitespace after a token parser.
+-- Kept internal so non-terminal grammar rules cannot mistakenly call it.
+lexeme :: TipParser a -> TipParser a
+lexeme p = p <* ws
+
+-- Terminals / Lexemes
+
+symbol :: String -> TipParser String
+symbol s = lexeme (string s <?> s)
+
+keyword :: String -> TipParser String
+keyword s = lexeme (string s <?> s)
+
+char :: Char -> TipParser Char
+char c = lexeme (satisfy (== c) <?> [c])
+
+-- Raw identifier (used for testing or as primitive)
+identifierP :: TipParser String
+identifierP = (:) <$> satisfy isAlpha <*> satisfyWhile (\x -> isDigit x || isAlpha x) <?> "an identifier"
+
+-- Lexeme identifier (consumes trailing whitespace)
+identifier :: TipParser String
+identifier = lexeme identifierP
+
+intLit :: TipParser Int
+intLit = lexeme (read <$> ((:) <$> satisfy isDigit <*> satisfyWhile isDigit) <?> "an integer")
+
+-- Enclosure and punctuation helpers
+
+parens :: TipParser a -> TipParser a
+parens p = char '(' *> p <* char ')'
+
+braces :: TipParser a -> TipParser a
+braces p = char '{' *> p <* char '}'
+
+semi :: TipParser Char
+semi = char ';'
+
+comma :: TipParser Char
+comma = char ','
+
+eof :: TipParser ()
+eof = Parser $ \st@(CharParserState ch pos) -> pure $ case ch of
+  "" -> ParseOk ((), st)
+  _ -> ParseError Empty pos (singleton "end of file")
