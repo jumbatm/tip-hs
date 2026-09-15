a805a0538bfc98b074beb00c6d702bbe991233a7
jumbatm <jumbatm@gmail.com>
2026-09-15 11:14:59 +1000

    WIP: Make short-circuiting Functor

--- src/Interpreter.hs
+++ src/Interpreter.hs
@@ -7,20 +7,17 @@ data Value = Integer Int | String String | Record (Map String Value)
 
 type Error = String
 
-type State = Either Error (Map String Value)
-
 -- TODO: Automatically derive from StateT
-newtype Interpreter a = Interpreter {run :: State -> (a, State)}
+newtype Interpreter a = Interpreter {runInterpreter :: Either Error (Map String Value -> (a, Map String Value))}
 
 instance Functor Interpreter where
-  fmap f interp = Interpreter $ \st -> let (v, st') = run interp st in (f v, st')
+  fmap f (Interpreter frun) = Interpreter $ (\run st -> let (v, st') = run st in (f v, st')) <$> frun
 
 instance Applicative Interpreter
 
-instance Monad Interpreter where
-  return v = Interpreter $ \st -> (v, st)
-
-  mv >>= mf = Interpreter $ \st ->
-    let (v, vs) = run mv st
-        (f, fs) = run (mf v) vs
-     in (f, fs)
+-- instance Monad Interpreter where
+--  return v = Interpreter $ \st -> (v, st)
+--
+--  mv >>= mf = Interpreter $ \st ->
+--    let (v, vs) = run mv st
+--     in run (mf v) vs
