b6d9b9aba7c9fc7dbc946790df8d28e6e6a8577c
jumbatm <jumbatm@gmail.com>
2026-09-15 20:54:38 +1000

    Implement binary expression evaluation

--- src/Interpreter.hs
+++ src/Interpreter.hs
@@ -3,9 +3,9 @@ module Interpreter where
 import Data.Bifunctor
 import Data.Map (Map)
 import qualified Data.Map as Map
-import Parser.TipParser
+import qualified Parser.TipParser as TP
 
-data Value = Integer Int | String String | Record (Map String Value)
+data Value = Integer Int | String String | Record (Map String Value) deriving (Show)
 
 type Error = String
 
@@ -34,3 +34,29 @@ get var = Interpreter $ \st -> case Map.lookup var st of
 
 put :: String -> Value -> Interpreter ()
 put var value = Interpreter $ \st -> Right ((), Map.insert var value st)
+
+panic :: String -> Interpreter a
+panic = Interpreter . const . Left
+
+-- TODO: Alas, again, the allure of using recursion scheme. Must... resist...
+evalExpr :: TP.Expression -> Interpreter Value
+evalExpr (TP.Int i) = pure $ Integer i
+evalExpr (TP.Id name) = get name
+evalExpr (TP.Binary op lhs rhs) = do
+  l <- evalExpr lhs
+  r <- evalExpr rhs
+  evalBinOp op l r
+  where
+    evalBinOp bop (Integer a) (Integer b) = pure . Integer $ case bop of
+      TP.Add -> a + b
+      TP.Subtract -> a - b
+      TP.Multiply -> a * b
+      TP.Divide -> a `div` b
+      TP.GreaterThan -> if a > b then 1 else 0
+      TP.Equal -> if a == b then 1 else 0
+    evalBinOp _ _ _ = panic $ "type mismatch: no " ++ show op ++ " defined on " ++ show lhs ++ " and " ++ show rhs
+evalExpr (TP.Unary op v) = undefined
+evalExpr (TP.Call f args) = undefined
+evalExpr (TP.Alloc expr) = undefined
+evalExpr (TP.Record bindings) = undefined
+evalExpr (TP.RecordAccess r field) = undefined
