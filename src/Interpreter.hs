d9316f480973a26ca4c7305175f8d773eeb6f740
jumbatm <jumbatm@gmail.com>
2026-09-20 21:51:12 +1000

    Swap to using IORef
    Since we'll need IO eventually anyway (for `input` and `output`
    statements), start using IORef.
    
    Map references directly to an IORef. Then we can rely on the native
    language's own GC to collect our references for us.

--- src/Interpreter.hs
+++ src/Interpreter.hs
@@ -1,42 +1,58 @@
 module Interpreter where
 
-import Data.Bifunctor
+import Data.IORef
 import Data.Map (Map)
 import qualified Data.Map as Map
 import qualified Parser.TipParser as TP
 
-data Value = Integer Int | String String | Record (Map String Value) | Cell String deriving (Show)
+data Value = Integer Int | String String | Record (Map String Value) | Cell (IORef Value) | Undef
 
 type Error = String
 
--- TODO: Automatically derive from StateT
-newtype Interpreter a = Interpreter {run :: Map String Value -> Either Error (a, Map String Value)}
+newtype Env = Env (Map String (IORef Value))
+
+newtype Interpreter a = Interpreter {run :: Env -> IO (Either Error a)}
 
 instance Functor Interpreter where
-  fmap f (Interpreter fv) = Interpreter $ fmap (first f) <$> fv
+  fmap f (Interpreter fv) = Interpreter $ \env -> fmap (fmap f) (fv env)
 
 instance Applicative Interpreter where
-  pure v = Interpreter $ \st -> Right (v, st)
-  (Interpreter interpf) <*> (Interpreter interpv) = Interpreter $ \s -> do
-    (f, fs) <- interpf s
-    (a, vs) <- interpv fs
-    Right (f a, vs)
+  pure v = Interpreter $ \_ -> pure $ Right v
+  (Interpreter interpf) <*> (Interpreter interpv) = Interpreter $ \env -> do
+    ef <- interpf env
+    case ef of
+      Left err -> pure $ Left err
+      Right f -> fmap f <$> interpv env
 
 instance Monad Interpreter where
   return = pure
 
-  (Interpreter iv) >>= f = Interpreter $ \st -> iv st >>= (\(a, st') -> run (f a) st')
+  (Interpreter iv) >>= fm = Interpreter $ \env -> do
+    ev <- iv env
+    case ev of
+      Left err -> pure . Left $ err
+      Right v -> run (fm v) env
+
+getAddr :: String -> Interpreter (IORef Value)
+getAddr var = Interpreter $ \(Env m) -> pure $ case Map.lookup var m of
+  Nothing -> Left $ "invalid variable " ++ var
+  Just v -> Right v
 
 get :: String -> Interpreter Value
-get var = Interpreter $ \st -> case Map.lookup var st of
-  Nothing -> Left $ "invalid variable " ++ var
-  Just v -> Right (v, st)
+get var = Interpreter $ \env -> do
+  eref <- run (getAddr var) env
+  case eref of
+    Left err -> pure $ Left err
+    Right ref -> Right <$> readIORef ref
 
 put :: String -> Value -> Interpreter ()
-put var value = Interpreter $ \st -> Right ((), Map.insert var value st)
+put var value = Interpreter $ \env@(Env m) -> do
+  case Map.lookup var m of
+    Nothing -> run (panic $ "no variable named " ++ var ++ " in scope") env
+    Just ref -> Right <$> writeIORef ref value
 
 panic :: String -> Interpreter a
-panic = Interpreter . const . Left
+panic = Interpreter . const . pure . Left
 
 -- TODO: Alas, again, the allure of using recursion scheme. Must... resist...
 evalExpr :: TP.Expression -> Interpreter Value
@@ -56,14 +72,20 @@ evalExpr (TP.Binary op lhs rhs) = do
       TP.Equal -> if a == b then 1 else 0
     evalBinOp _ _ _ = panic $ "no " ++ show op ++ " defined on " ++ show lhs ++ " and " ++ show rhs
 evalExpr (TP.Unary TP.Dereference (TP.Id name)) = get name
-evalExpr (TP.Unary TP.AddressOf (TP.Id name)) = pure $ Cell name
+evalExpr (TP.Unary TP.AddressOf (TP.Id name)) = do
+  addr <- getAddr name
+  pure $ Cell addr
 evalExpr (TP.Unary op v) = do
   x <- evalExpr v
   evalUnOp op x
   where
     evalUnOp TP.Negate (Integer n) = pure $ Integer (-n)
     evalUnOp _ _ = panic $ "no " ++ show op ++ " defined for " ++ show v
+evalExpr (TP.Alloc expr) = Interpreter $ \env -> do
+  ev <- run (evalExpr expr) env
+  case ev of
+    Left err -> pure $ Left err
+    Right v -> Right . Cell <$> newIORef v
 evalExpr (TP.Call f args) = undefined
-evalExpr (TP.Alloc expr) = undefined
 evalExpr (TP.Record bindings) = undefined
 evalExpr (TP.RecordAccess r field) = undefined
